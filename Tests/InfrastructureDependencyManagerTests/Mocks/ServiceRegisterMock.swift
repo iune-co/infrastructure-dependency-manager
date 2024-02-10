0a2c785f06a0fee715ded46a94fd4fa6dba678fb
Tarek Abdallah <tarekabd1995@gmail.com>
2024-02-11 00:26:41 +0100

    Update ServiceRegisterMock.swift

--- Tests/InfrastructureDependencyManagerTests/Mocks/ServiceRegisterMock.swift
+++ Tests/InfrastructureDependencyManagerTests/Mocks/ServiceRegisterMock.swift
@@ -2,7 +2,7 @@
 
 final class ServiceRegistrarMock: ServiceRegistrar {
     private(set) var registerMethodWasCalled: Bool = false
-    func register(on container: DependencyManager)
+    func register(on manager: DependencyManager)
     {
         registerMethodWasCalled = true
     }
